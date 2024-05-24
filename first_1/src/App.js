89c17ddf5e6a73d1387f1850fd2b9ac64189b609
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-05-24 23:04:20 +0100

    update

--- first_1/src/App.js
+++ first_1/src/App.js
@@ -5,23 +5,23 @@ const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setCount((prCount) => prCount + 1);
-        }}
-      >
-        {" "}
-        +{" "}
-      </button>{" "}
-      <h1> {count} </h1>{" "}
-      <button
-        onClick={() => {
-          setCount((ncount) => ncount - 1);
-        }}
-      >
-        {" "}
-        -{" "}
-      </button>{" "}
+      {/* <button
+                    onClick={() => {
+                      setCount((prCount) => prCount + 1);
+                    }}
+                  >
+                    {" "}
+                    +{" "}
+                  </button>{" "}
+                  <h1> {count} </h1>{" "}
+                  <button
+                    onClick={() => {
+                      setCount((ncount) => ncount - 1);
+                    }}
+                  >
+                    {" "}
+                    -{" "}
+                  </button>{" "} */}{" "}
     </div>
   );
 };
